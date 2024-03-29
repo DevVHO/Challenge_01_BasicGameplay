07fa09fb7160e4e52bbda1cf3de7bf37ae574382
DevVHO <a53553@alunos.ipb.pt>
2024-03-29 22:34:23 +0000

    Lesson_2.1
    Player_Positoning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalinput;
+    private float Rangex = 15f;
+    private float speed = 25.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalinput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right*horizontalinput*Time.deltaTime*speed); 
+        if(transform.position.x > Rangex)
+        {
+            transform.position = new Vector3(Rangex,transform.position.y,transform.position.z);
+        }
+        if (transform.position.x < -Rangex)
+        {
+            transform.position = new Vector3(-Rangex, transform.position.y, transform.position.z);
+        }
     }
 }
