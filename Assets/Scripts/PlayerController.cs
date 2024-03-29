1f8551bcd95617ad26565b6f58924347255a826c
DevVHO <a53553@alunos.ipb.pt>
2024-03-29 23:54:46 +0000

    Lesson_2.2
    In This step i am making some scripts for my game, so i created script how for example movefoward and destroy(beyond of learn the Initiable)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject LaunchObj;
     public float horizontalinput;
     private float Rangex = 15f;
     private float speed = 25.0f;
@@ -16,6 +17,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        #region movementplayer
         horizontalinput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right*horizontalinput*Time.deltaTime*speed); 
         if(transform.position.x > Rangex)
@@ -25,6 +27,13 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x < -Rangex)
         {
             transform.position = new Vector3(-Rangex, transform.position.y, transform.position.z);
+            
+        }
+        #endregion
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(LaunchObj,transform.position,LaunchObj.transform.rotation);
         }
     }
 }
